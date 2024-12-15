5f3ec95e3fcec1ab34a5164f31651e72a318526c
Abdul Aziz Khan <abdulazizk811@gmail.com>
2024-12-16 02:54:19 +0500

    Services Page Completed

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -9,18 +9,71 @@ const Services = () => {
         <br /> Manufacturing Services
       </h1>
       <p>Simplify operations with our efficient and quality-focused service</p>
-      <div className="flex">
-        <div className="h-56 bg-gray-900 w-72 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-20 shadow-lg">
-          <div className="flex justify-center items-center">
-            <SunMedium />
-            <ArrowUpRight />
+      <div className="sm:flex justify-center items-center gap-12 p-6 max-w-[95%] mx-auto">
+        <div className="h-56 py-6 bg-gray-900 px-3 w-80 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-20 shadow-lg">
+          <div className="flex justify-between items-center">
+            <SunMedium size={30} />
+            <ArrowUpRight size={30} />
             </div>
-            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
-            <p>
+            <h1 className="capitalize font-bold tracking-wide mt-12">production and assembly</h1>
+            <p className="text-sm font-[light] mt-2">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
             </p>
-          
         </div>
+        <div className="h-56 py-6 bg-gray-900 px-3 w-80 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-20 shadow-lg">
+          <div className="flex justify-between items-center">
+            <SunMedium size={30} />
+            <ArrowUpRight size={30} />
+            </div>
+            <h1 className="capitalize font-bold tracking-wide mt-12">production and assembly</h1>
+            <p className="text-sm font-[light] mt-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+            </p>
+        </div>
+        <div className="h-56 py-6 bg-gray-900 px-3 w-80 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-20 shadow-lg">
+          <div className="flex justify-between items-center">
+            <SunMedium size={30} />
+            <ArrowUpRight size={30} />
+            </div>
+            <h1 className="capitalize font-bold tracking-wide mt-12">production and assembly</h1>
+            <p className="text-sm font-[light] mt-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+            </p>
+        </div>
+       
+      </div>
+      <div className="flex justify-center items-center gap-12 p-6 max-w-[95%] mx-auto">
+        <div className="h-56 py-6 bg-gray-900 px-3 w-80 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-20 shadow-lg">
+          <div className="flex justify-between items-center">
+            <SunMedium size={30} />
+            <ArrowUpRight size={30} />
+            </div>
+            <h1 className="capitalize font-bold tracking-wide mt-12">production and assembly</h1>
+            <p className="text-sm font-[light] mt-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+            </p>
+        </div>
+        <div className="h-56 py-6 bg-gray-900 px-3 w-80 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-20 shadow-lg">
+          <div className="flex justify-between items-center">
+            <SunMedium size={30} />
+            <ArrowUpRight size={30} />
+            </div>
+            <h1 className="capitalize font-bold tracking-wide mt-12">production and assembly</h1>
+            <p className="text-sm font-[light] mt-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+            </p>
+        </div>
+        <div className="h-56 py-6 bg-gray-900 px-3 w-80 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-20 shadow-lg">
+          <div className="flex justify-between items-center">
+            <SunMedium size={30} />
+            <ArrowUpRight size={30} />
+            </div>
+            <h1 className="capitalize font-bold tracking-wide mt-12">production and assembly</h1>
+            <p className="text-sm font-[light] mt-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+            </p>
+        </div>
+       
       </div>
     </div>
   );
